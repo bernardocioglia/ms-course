583749075f1f99b7a7ee6709ffc6abfe2b6616f8
Bernardo Dantas <bernardo.cioglia@gmail.com>
2023-09-13 15:28:14 -0300

    Project hr-gateway-api created

---
+++ hr-gateway-api/src/main/java/com/devsuperior/hrgatewayapi/HrGatewayApiApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hrgatewayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrGatewayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrGatewayApiApplication.class, args);
+	}
+
+}
---
+++ hr-gateway-api/src/test/java/com/devsuperior/hrgatewayapi/HrGatewayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hrgatewayapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrGatewayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
